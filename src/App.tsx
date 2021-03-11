3fd2e1ea39e119a0387151621eeaf797e834cd43
Alexey <61068421+Aloxan12@users.noreply.github.com>
2021-03-11 17:35:29 +0300

    My the social network

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Setting from './components/Setting/Setting';
+import { BrowserRouter, Route } from "react-router-dom";
+import {StoreType} from "./redux/state";
 
-function App() {
-  return (<div className="app-wrapper">
-   <Header />
-   <Navbar />
-   <Profile />
-  </div>
-  );
+export type AppType = {
+  store: StoreType
+  //state: StateType
+  //addPostCallback:(m:string)=>void
+  //updateYourPostText: (t:string)=>void
+  //dispatch:(action: ActionType)=> void
+}
+
+const App: React.FC<AppType> = (props) => {
+  const state = props.store.getState();
+  return <BrowserRouter>
+    <div className="app-wrapper">
+      <Header />
+      <Navbar state={state.navbarPage}/>
+      <div className="app-wrapper-content">
+        <Route path='/profile' render={() => <Profile state={state.profilePage}
+                                                      addPostCallback={props.store.addPostCallback.bind(props.store)}
+                                                      message={state.profilePage.newPostText}
+                                                      updateYourPostText={props.store.updateYourPostText.bind(props.store)}
+                                                      dispatch={props.store.dispatch.bind(props.store)}
+                                              />} />
+        <Route path='/dialogs' render={() => <Dialogs state={props.store._state.dialogsPage}
+                                                      dispatch={props.store.dispatch.bind(props.store)}
+                                                      message={state.dialogsPage.newMessage}
+        />}  />
+        <Route path='/music' render={() => <Music />} />
+        <Route path='/news' render={() => <News />} />
+        <Route path='/setting' render={() => <Setting />} />
+      </div>
+    </div>
+  </BrowserRouter>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StateType} from "./redux/state";
+import ReactDOM from "react-dom";
+import App from "./App";
+import store from "./redux/state";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export let renderEntireTree =()=>{
+    ReactDOM.render(
+        <React.StrictMode>
+            <App store={store} />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+
+renderEntireTree()
+
+store.subscribe(renderEntireTree)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
