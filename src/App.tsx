b6dee3e781d04f0caeb4e76e4288be17ca3797ca
Aloxan12 <kvakvaaak@mail.ru>
2023-06-16 11:42:19 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
         <MapPopup />
-    </div>
+          </div>
   )
 }
 
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,22 +1,32 @@
 import React, {useEffect, useState} from 'react';
-import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+import {YMaps, Map, Placemark} from '@pbe/react-yandex-maps';
 import './Map.scss'
+import { useSearchParams } from 'react-router-dom'
 
 export const MapBlock = () => {
+    const [searchParams, setSearchParams] = useSearchParams()
     const [currentCoordinates, setCurrentCoordinates] = useState([55.751574, 37.573856]);
     const coordinatesList = [
         [55.751574, 37.573856], // Координаты первого маркера
         [55.75222, 37.615555], // Координаты второго маркера
     ];
 
+    const handleMouseEnter = (id: number) => {
+        searchParams.set('shop', `${90}`)
+        setSearchParams(searchParams.toString())
+    };
+
+    (window as any).handleMouseEnter = handleMouseEnter;
+
     return (
         <YMaps>
             <Map
-                defaultState={{ center: currentCoordinates, zoom: 15 }}
+                defaultState={{center: currentCoordinates, zoom: 15}}
                 modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
                 width="100%"
                 height="400px"
             >
+                <div className='map-bg'>
                 {coordinatesList.map((coordinates, index) => (
                     <Placemark
                         key={index}
@@ -31,15 +41,22 @@ export const MapBlock = () => {
                                 <span><b>Пн — Сб:</b> с 8:00 до 21:30</span>
                                 <span><b>Вс:</b> с 9:00 до 20:00</span>
                             </div>
-                            <div class="btn">Выбрать</div>
+                            <div
+                    class="btn"
+                    onclick="handleMouseEnter(${index})"
+                  >
+                    Выбрать
+                  </div>
                             </div>`,
                         }}
                         options={{
                             balloonPanelMaxMapArea: 0,
                         }}
                         modules={['geoObject.addon.balloon']}
-                    />
+                    >
+                    </Placemark>
                 ))}
+                </div>
             </Map>
         </YMaps>
     );
